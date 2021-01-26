09abc51aff946d5db45377a4329886332b698b4f
gagnonsilas <gagnon.silas@gmail.com>
2021-01-25 22:46:02 -0800

    Initial Commit

---
+++ desktop/src/analogsnigs/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package analogsnigs.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import analogsnigs.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
